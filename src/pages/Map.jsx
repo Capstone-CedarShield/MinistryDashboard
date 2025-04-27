e96ebe775675a3d297f71a139fea172343622b92
Alexkheir <121764981+Alexkheir@users.noreply.github.com>
2025-04-27 12:41:56 +0300

    Update Map.jsx

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -20,7 +20,7 @@ const lebanonBounds = {
 const Map = ({ incidents }) => {
 
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: "AIzaSyBXGGBbpCxSdardQw9h_BlN6EwpGgARxTg",
+    googleMapsApiKey: "",
     libraries,
   });
 
@@ -35,7 +35,7 @@ const Map = ({ incidents }) => {
       const response = await fetch(
         `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
           address
-        )}&key=AIzaSyBXGGBbpCxSdardQw9h_BlN6EwpGgARxTg`
+        )}&key=`
       );
       const data = await response.json();
       if (data.results && data.results.length > 0) {
